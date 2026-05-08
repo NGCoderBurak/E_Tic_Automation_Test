aad67ca5e5b1dfb8fce2e335e8a86034de4c7e95
PEGASUS <kamilburak78@gmail.com>
2026-05-08 22:02:32 +0300

    Login Deneme Commiti

--- src/Master/E_Tic_Automation.java
+++ src/Master/E_Tic_Automation.java
@@ -1,15 +1,24 @@
 package Master;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class E_Tic_Automation extends BaseDriver {
-    @Test
+    @Test (priority=1)
     public void loginTest()
     {
+        driver.get("https://automationexercise.com");
+        WebElement login=driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a"));
+        login.click();
 
-
-
+        WebElement username=driver.findElement(By.id("username"));
+        username.sendKeys("admin");
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("admin");
+        WebElement submit=driver.findElement(By.id("submit"));
+        submit.click();
 
     }
 
