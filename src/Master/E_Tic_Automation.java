096085c65acd83269db4e139960ed413be57b407
PEGASUS <kamilburak78@gmail.com>
2026-05-08 20:53:51 +0300

    Initial commit

---
+++ src/Master/E_Tic_Automation.java
@@ -0,0 +1,16 @@
+package Master;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class E_Tic_Automation extends BaseDriver {
+    @Test
+    public void loginTest()
+    {
+
+
+
+
+    }
+
+}
